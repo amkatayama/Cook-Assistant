9a67e6a15929a4c78d208855ba07ea00e6df1d09
aratakatayama <99364243+aratakatayama@users.noreply.github.com>
2022-11-23 13:25:58 -0500

    Initial Commit

---
+++ Cook Assistant/Cook_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cook_AssistantApp.swift
+//  Cook Assistant
+//
+//  Created by Arata Michael Katayama on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Cook_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
