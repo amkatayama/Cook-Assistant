3e97dc647ee6464ab7ba8628535b77516242e8e2
aratakatayama <99364243+aratakatayama@users.noreply.github.com>
2022-11-30 11:24:51 -0500

    updating Webscrape algorithm

--- Cook Assistant/WecscrapeScript.swift
+++ Cook Assistant/WecscrapeScript.swift
@@ -9,6 +9,44 @@ import SwiftUI
 import Firebase
 import SwiftSoup
 
+struct Webscrape {
+    
+}
+
+class Recipe {
+    
+    var name: String
+    var description: String
+    var image: String
+    var ingredient: [String]
+    var nutrition: [String]
+    
+    init(name: String, description: String, image: String, ingredient: [String], nutrition: [String]) {
+        self.name = name
+        self.description = description
+        self.image = image
+        self.ingredient = ingredient
+        self.nutrition = nutrition
+    }
+    
+    // getters method
+    func getName() -> String {
+        return self.name
+    }
+    func getDescription() -> String {
+        return self.description
+    }
+    func getImage() -> String {
+        return self.image
+    }
+    func getIngredient() -> [String] {
+        return self.ingredient
+    }
+    func getNutrition() -> [String] {
+        return self.nutrition
+    }
+}
+
 struct WebscrapeView: View {
     
     // creating global variables for necessary html tags
@@ -24,26 +62,28 @@ struct WebscrapeView: View {
 //    @State var recipeIngredDict: [String: [String]] = [:]  // ingred info
     
     // linking to Firebase
-    func addToFirestore() {
-
-    //    accessSubURL()
-        var recipeData = getRecipeData()
-
-        // creating a constant for the database
-        let db = Firestore.firestore()
-
-        // loop through the number of links retrieved to create the correct number of documents
-        // add all information to firestore
-        for i in 0..<recipeData.rn.count {
-t            db.collection("recipeData").addDocument(data: ["name": recipeData.rn[i], "info": recipeData.rd[i], "image": recipeData.ri[i]])
-        }
-
-    }
+//    func addToFirestore() {
+//
+//    //    accessSubURL()
+//        var recipeData = getRecipeData()
+//
+//        // creating a constant for the database
+//        let db = Firestore.firestore()
+//
+//        // loop through the number of links retrieved to create the correct number of documents
+//        // add all information to firestore
+//        for i in 0..<recipeData.rn.count {
+//            db.collection("recipeData").addDocument(data: ["name": recipeData.rn[i], "info": recipeData.rd[i], "image": recipeData.ri[i]])
+//        }
+//
+//    }
+    
+    // global variable prevents function calls inside another function
+    // better time complexity
+    @State var subUrlList: [String] = []
     
     // function that extracts every recipe link from the main page of the website
-    func getSubUrls(n: Int) -> [String]? {  // n is the page number of baseurl
-        
-        var subUrlList: [String] = []
+    func getSubUrls(n: Int) {  // n is the page number of baseurl
         
         // URL of each page in allrecipes.com
         let baseUrl = "https://www.allrecipes.com/recipes/1947/everyday-cooking/quick-and-easy/?page=\(n)"
@@ -51,7 +91,7 @@ t            db.collection("recipeData").addDocument(data: ["name": recipeData.r
         // if baseUrl cannot be found or accessed
         guard let accessBaseUrl = URL(string: baseUrl) else {
             print("Invalid URL Error: \(baseUrl)")
-            return nil // break from the function
+            return // break from the function
         }
         
         // retrieve the html code for accessBaseUrl
@@ -76,81 +116,82 @@ t            db.collection("recipeData").addDocument(data: ["name": recipeData.r
         } catch let error {
             print(error)
         }
-        
-        return subUrlList
     }
     
-    func getRecipeData() -> (rn: [String], rd: [String], ri: [String]) {  // optional string
-        
-        // declared outside to use less memory
-        var recipeNames: [String] = []
-        var recipeDescriptions: [String] = []
-        var recipeImages: [String] = []
-        
-        // for each page access all suburls and retrieve recipe data
-        for n in 0..<3 {
-            
-            // for each iteration recipeLinks will contain the current subURL list
-            var recipeLinks = getSubUrls(n: n)
-            
-            if let unwrapped = recipeLinks {  // if recipeLinks is not nil
-                for subUrl in unwrapped {  // subUrlList contains recipe links for 1 whole page
-                    // access each subURLs and handle error
-                    guard let accessSubUrl = URL(string: subUrl) else {
-                        print("Invalid URL Error: \(subUrl)")
-                        continue // skip iteration
-                    }
-                    
-                    // retrieve the html code for accessSubUrl
-                    do {
-                        let subHtmlString = try String(contentsOf: accessSubUrl, encoding: .ascii)
-                        do {
-                            var doc: Document = try SwiftSoup.parse(subHtmlString)
-                            // inserting every "h1" tag into array sublink
-                            var allHeader: [Element] = try doc.select("h1").array()
-                            // inserting every "p" tag into array sublink
-                            var allParagraphs: [Element] = try doc.select("p").array()
-                            // inserting every "div" tag into array sublink
-                            var allImages: [Element] = try doc.select("img").array()
-                            // extracting recipe title
-                            for i in 0..<allHeader.count {
-                                let classNameTitle: String = try allHeader[i].className()
-                                let classNameDescription: String = try allParagraphs[i].className()
-                                let classNameImage: String = try allImages[i].className()
-                                // className is susceptible to change
-                                if classNameTitle == "comp type--lion article-heading mntl-text-block" {
-                                    let rName: String = try allHeader[i].text()
-                                    recipeNames.append(rName)
-                                    print("Successful")
-                                }
-                                // className is susceptible to change
-                                if classNameDescription == "comp type--dog article-subheading" {
-                                    let rDescription: String = try allParagraphs[i].text()
-                                    recipeDescriptions.append(rDescription)
-                                    print("Successful")
-                                }
-                                // className is susceptible to change
-                                if classNameImage == "universal-image__image lazyloaded" {
-                                    let rImage: String = try allImages[i].attr("data-src")
-                                    recipeImages.append(rImage)
-                                    print("Successful")
-                                }
-                            }
-                            
-                        } catch Exception.Error(type: let type, Message: let message){
-                            print("Error: \(type), \(message)")
-                        }
-                    } catch let error {
-                        print(error)
-                    }
-                }
-            } else {
-                continue
-            }
-        }
-        
-        return (recipeNames, recipeDescriptions, recipeImages)
-    }
+//    func getRecipeData() -> (rn: [String], rd: [String], ri: [String]) {  // optional string
+//
+//        // declared outside to use less memory
+//        var recipeNames: [String] = []
+//        var recipeDescriptions: [String] = []
+//        var recipeImages: [String] = []
+//
+//        // for each page access all suburls and retrieve recipe data
+//        for n in 0..<3 {
+//
+//            for subUrl in self.subUrlList {
+//
+//            }
+//
+//
+//
+//            if let unwrapped = recipeLinks {  // if recipeLinks is not nil
+//                for subUrl in unwrapped {  // subUrlList contains recipe links for 1 whole page
+//                    // access each subURLs and handle error
+//                    guard let accessSubUrl = URL(string: subUrl) else {
+//                        print("Invalid URL Error: \(subUrl)")
+//                        continue // skip iteration
+//                    }
+//
+//                    // retrieve the html code for accessSubUrl
+//                    do {
+//                        let subHtmlString = try String(contentsOf: accessSubUrl, encoding: .ascii)
+//                        do {
+//                            let doc: Document = try SwiftSoup.parse(subHtmlString)
+//                            // inserting every "h1" tag into array sublink
+//                            let allHeader: [Element] = try doc.select("h1").array()
+//                            // inserting every "p" tag into array sublink
+//                            let allParagraphs: [Element] = try doc.select("p").array()
+//                            // inserting every "div" tag into array sublink
+//                            let allImages: [Element] = try doc.select("img").array()
+//                            // extracting recipe title
+//                            for i in 0..<allHeader.count {
+//                                let classNameTitle: String = try allHeader[i].className()
+//                                let classNameDescription: String = try allParagraphs[i].className()
+//                                let classNameImage: String = try allImages[i].className()
+//                                // className is susceptible to change
+//                                if classNameTitle == "comp type--lion article-heading mntl-text-block" {
+//                                    let rName: String = try allHeader[i].text()
+//                                    recipeNames.append(rName)
+//                                    print("Successful")
+//                                }
+//                                // className is susceptible to change
+//                                if classNameDescription == "comp type--dog article-subheading" {
+//                                    let rDescription: String = try allParagraphs[i].text()
+//                                    recipeDescriptions.append(rDescription)
+//                                    print("Successful")
+//                                }
+//                                // className is susceptible to change
+//                                if classNameImage == "universal-image__image lazyloaded" {
+//                                    let rImage: String = try allImages[i].attr("data-src")
+//                                    recipeImages.append(rImage)
+//                                    print("Successful")
+//                                }
+//                            }
+//
+//                        } catch Exception.Error(type: let type, Message: let message){
+//                            print("Error: \(type), \(message)")
+//                        }
+//                    } catch let error {
+//                        print(error)
+//                    }
+//                }
+//            } else {
+//                continue
+//            }
+//        }
+//
+//        return (recipeNames, recipeDescriptions, recipeImages)
+//    }
     
     var body: some View {
         
@@ -160,7 +201,13 @@ t            db.collection("recipeData").addDocument(data: ["name": recipeData.r
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Button ("Hello, world!") {
-                addToFirestore()
+                // build subUrlList
+                for i in 0..<100 {
+                    getSubUrls(n: i)
+                }
+                // for each subUrl
+                
+                print(subUrlList)
             }
         }
         .padding()
